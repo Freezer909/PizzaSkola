e06309dd14581aa2af00af3fe4da56eceaeacae1
Viktors Luks <luks.viktors@gmail.com>
2026-01-13 10:44:22 +0200

    Pieliku klat savu projektu

---
+++ src/main/GamePanel.java
@@ -0,0 +1,125 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+import entity.Player;
+import tile.TileManager;
+
+public class GamePanel extends JPanel implements Runnable {
+
+	//SCREEN SETTING
+	final int originalTileSize=16; //16x16 tile
+	final int scale = 3 ;
+	
+	public final int tileSize = originalTileSize * scale; //48*48
+	public final int maxScreenCol = 20;
+	public final int maxScreenRow = 12;
+	public final int screenWidth = tileSize * maxScreenCol;  //960pix
+	public final int screenHeight = tileSize * maxScreenRow; //576pix
+	
+	//WORLD SETTINGS
+	public final int maxWorldCol = 50;
+	public final int maxWorldRow = 50;
+	public final int worldWidth = tileSize * maxWorldCol;
+	public final int worldHeight = tileSize * maxWorldRow;
+	
+	//FPS
+	int FPS=60;
+	
+	//GAME STATE
+	public int gameState;
+	public final int playState = 1;
+	public final int pauseState = 2;
+	
+	
+	//System
+	Sound sound = new Sound();
+	TileManager tileM = new TileManager(this);
+	KeyHandler keyH = new KeyHandler();
+	Thread gameThread;
+	public CollisionChecker cChecker = new CollisionChecker(this);
+	
+	
+	//PLayers
+	public Player player = new Player(this,keyH);	
+	
+	public GamePanel(){
+		
+		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+		this.setBackground(Color.black);
+		this.setDoubleBuffered(true);
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
+	}
+
+	public void startGameThread() {
+		
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+	
+	
+	@Override
+	public void run() {
+		
+		double drawInterval = 1000000000/FPS; //0.01666 
+		double nextDrawTime = System.nanoTime() + drawInterval;
+		
+		while(gameThread != null) {
+			
+			update();
+			 
+			
+
+			repaint();
+			
+			
+			try {
+				double remainingTime = nextDrawTime - System.nanoTime();
+				remainingTime = remainingTime/1000000;
+				
+				if(remainingTime < 0) {
+					remainingTime = 0;
+				}
+				
+				Thread.sleep((long) remainingTime);
+				
+				nextDrawTime += drawInterval;
+				
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
+	
+	public void update() {
+		
+		player.update();
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+		super.paintComponent(g);
+		
+		Graphics g2 = (Graphics2D)g;
+		
+		tileM.draw((Graphics2D) g2);
+		
+		player.draw(g2);
+		
+		g2.dispose();
+	}
+	
+}
+
+
+
+
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,6 @@
  * 
  */
 module Picerija {
+	requires java.desktop;
+	requires ChatGptGame;
 }
